4b27a22a2ff7d96517bdbbea6e6075f98c03fea7
arash-87 <86028430+arash-87@users.noreply.github.com>
2022-01-10 19:31:05 +1000

    Update core.py

--- core.py
+++ core.py
@@ -47,28 +47,37 @@ def Core(filepath, ssl):
                 SRV_UP = True if os.system("ping -n 2 " + server + "> NUL") == 0 else False
                 
                 # First condition triggered if a router is reachable and based on SSL
-                if SRV_UP == True and ssl == True:
+                if SRV_UP == True:
                     print("\n> Connecting to the router...\n")
                     
                     try:
-                        # Estblish a connection to each router but it is based digital certificate installed on each router!       
-                        connection = routeros_api.RouterOsApiPool(server, user, passw, plaintext_login=True, use_ssl=True, ssl_verify_hostname=False, ssl_verify=True, port=8729)
+					
+						if ssl == True:
+
+							# Estblish a connection to each router but it is based digital certificate installed on each router!       
+							connection = routeros_api.RouterOsApiPool(server, user, passw, plaintext_login=True, use_ssl=True, ssl_verify_hostname=False, ssl_verify=True, port=8729)
+						
+						
+						if ssl == False:
+						
+							# Estblish a connection to each router from "SERVERS" but based on plaintext!       
+							connection = routeros_api.RouterOsApiPool(server, user, passw, plaintext_login=True, port=8728)
+							
                         api = connection.get_api()
                         print(">> Connection has been established\n")
                         try:
+						
                             # Run a MikroTik command
                             list_hotspot = api.get_resource('/ip/hotspot')
                             
                             # Get data from a command based on the ID
-                            output = list_hotspot.get(id='*1') [0]["id"] # To see all the parametrs of /ip/hotspot, remove everything in () and the following
+                            output = list_hotspot.get(id='*1') [0]["id"] 
+							# To see all the parametrs of /ip/hotspot, remove everything in () and the following
                             print(">>> Hotspot ID: ", output) 
                             
                             # Set new data to router(s) using the parameter of "disbaled"
                             # true: hotsput disable // false: enable hotspot
-                            # Must uncomment your line
-							
-							#list_hotspot.set(id=output,addresses_per_mac='2') 
-                            #list_hotspot.set(id=output,disabled='false')
+                            list_hotspot.set(id=output,disabled='true')
                             print("\n>>>> Hotspot has been disabled\n")
                             
                             # Close the connection and clear the array of string
@@ -95,56 +104,7 @@ def Core(filepath, ssl):
                         f.write("\nInvalid User/Password or port (8729) isn't open: {} | {} \n".format(name, server))
                         f.close()
                         String.clear() 
-                        print("=====================================================================================\n\n")		
-
-                # Second condition triggered if a router is reachable but not based on SSL
-                if SRV_UP == True and ssl == False:
-                    print("\n> Connecting to the router...\n")
-                    
-                    try:
-                        # Estblish a connection to each router from "SERVERS" but based on plaintext!       
-                        connection = routeros_api.RouterOsApiPool(server, user, passw, plaintext_login=True, port=8728)
-                        api = connection.get_api()
-                        print("> Connection has been established")
-                        try:
-                            # Run a MikroTik command
-                            list_hotspot = api.get_resource('/ip/hotspot')
-                            # Get data from a command based on the ID
-                            output = list_hotspot.get(id='*1') [0]["id"] # To see all the parametrs of /ip/hotspot, remove everything in () and the following
-                            print(">>> Hotspot ID: ", output)                                     
-
-							# Set new data to router(s) using the parameter of "disbaled"
-                            # true: hotsput disable // false: enable hotspot
-                            # Must uncomment your line
-							
-							#list_hotspot.set(id=output,addresses_per_mac='2') 
-                            #list_hotspot.set(id=output,disabled='false')
-                            print("\n>>>> Hotspot has been disabled\n")
-							
-                            connection.disconnect()
-                            String.clear()         
-                            print("=====================================================================================\n\n")                    
-                        
-                        # An exception that logs and prints when an error happned in try section
-                        except Exception as Argument:
-                            print(">>> The given argument is wrong or doesn't exist in your MikroTik: {} | {} \n".format(name, server))              
-                            # Print inside error.txt file if any errors for invalid credentials 
-                            f = open("logs.txt", "a")
-                            f.write("\nThe given argument is wrong or doesn't exist in your MikroTik: {} | {} \n".format(name, server))
-                            f.close()
-                            String.clear() 
-                            print("=====================================================================================\n\n")                         
-                        
-                    # An exception that logs and prints when an error happned in try section
-                    except Exception as Argument:
-                        print(">> Invalid User/Password or port (8728) isn't open: {} | {} \n".format(name, server))
-                        
-                        # Print inside error.txt file if any errors for invalid credentials 
-                        f = open("logs.txt", "a")
-                        f.write("\nInvalid User/Password or port (8728) isn't open: {} | {} \n".format(name, server))
-                        f.close()
-                        String.clear() 
-                        print("=====================================================================================\n\n")                        
+                        print("=====================================================================================\n\n")		          
 						        
                 # If there is no connectin to a gateway, print time out
                 if SRV_UP == False: 
@@ -160,6 +120,7 @@ def Core(filepath, ssl):
                     
 
 #################### File-check function #####################
+
 def filechk(filepath, ssl):
     # 1st check whether the file exists or not       
     if os.path.isfile(filepath): 
@@ -171,33 +132,36 @@ def filechk(filepath, ssl):
     else:  
         print("\n> No file with this name!", filepath)
         
-        
-######################## Main Menu #############################
+       
+#################### Menu #####################
+
+def menu():
+	print("\n###################           MikroTik API remote controller        #####################")
+	print("###################                                                 #####################")
+	print("                   |                  Site lists                    |")
+	print("                   |             1) Sites in Melbourne              |")
+	print("                   |             2) Sites in Sydney                 |")
+	print("                   |             3) Sites in Brisbane               |")
+	print("                   |             4) Sites in Adelaide               |")
+	print("                   |             5) Exit                            |")
+	print("                    ------------------------------------------------")   
+	
+	
+######################## Inputs #############################
 
 # This loop read data from keyboard
 while True:
 
-    print("\n###################           MikroTik API remote controller        #####################")
-    print("###################                                                 #####################")
-    print("                   |                  Site lists                    |")
-    print("                   |             1) Sites in Melbourne              |")
-    print("                   |             2) Sites in Sydney                 |")
-    print("                   |             3) Sites in Brisbane               |")
-    print("                   |             4) Sites in Adelaide               |")
-    print("                   |             5) Exit                            |")
-    print("                    ------------------------------------------------")
-    
+    menu()
     value = input("\n> Enter a number to apply change for .... :  ")
-	
-######################## Inputs #############################	
-    
+
     if value == '1':
         filepath = '.\ip-list.txt'
         ssl = True # If connection is based on SSL use port 8729
         filechk(filepath, ssl) # Call File-check function
             
     elif value == '2':
-        #filepath = '.\ip-list2.txt'
+        filepath = '.\ip-list2.txt'
         ssl = False # If connection is not based SSL use port 8728 
         filechk(filepath, ssl)  
 
